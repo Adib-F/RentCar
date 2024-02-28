96fad5a98acc3164173430575c8baa41ba0186af
Gutserk <ardanasmirza@gmail.com>
2024-02-29 00:02:01 +0700

    Register

---
+++ resources/views/register.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regiser</title>
+</head>
+<body>   
+<center>
+   
+   <table>
+    <h1>RENTCANG</h1>
+    <h4>Register</h1>
+    <tr>
+        <td>Nama Lengkap:</td>
+        <td><input type="text"></td>
+    </tr>
+    <tr>
+        <td>No.Telepon:</td>
+        <td><input type="text"></td>
+    </tr>
+    <tr>
+        <td>Alamat:</td>
+        <td><input type="text"></td>
+    </tr>
+    <tr>
+        <td>Email:</td>
+        <td><input type="text"></td>
+    </tr>
+    <tr>
+        <td>Password:</td>
+        <td><input type="text"></td>
+    </tr>
+
+    
+   </table>
+   </center>
+</body>
+</html>
