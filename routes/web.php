bf12e8f24bf6ac40037b78cdf736feeea960b2b6
Adib-F <fakhrimuhammadadib@gmail.com>
2024-02-29 09:40:02 +0700

    Menyiapkan Login view dan controller

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ListBarangController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 
+Route::get('/login', [LoginController::class, 'loginForm']);
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
-Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
 
 // routes/web.php
-route::get('/user/{id}', function ($id){
+Route::get('/user/{id}', function ($id){
     return 'User dengan ID'. $id;
 });
 
 // routes/web.php
-route::prefix('admin')->group(function (){
-    route::get('dashboard', function(){
+Route::prefix('admin')->group(function (){
+    Route::get('dashboard', function(){
         return 'Admin Dashboard';
     });
 
-    route::get('/users', function (){
+    Route::get('/users', function (){
         return 'Admin Users';
     });
 });
 
-// route::get('/listbarang/{id}/{nama}', function($id, $nama){
-//     return view('list_barang', compact('id', 'nama'));
-// });
-
 Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
-
-Route::get('/login', [LoginController::class, 'loginForm']);
