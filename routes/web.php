28ad3a7997429548fa065ad18b4a95e150ccc2bc
Adib-F <fakhrimuhammadadib@gmail.com>
2024-03-07 09:14:56 +0700

    Pembaruan Main

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ListBarangController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoginController;
 
-Route::get('/login', [LoginController::class, 'loginForm']);
 Route::get('/', function () {
-    return view('login');
+    return view('welcome');
 });
 
-Route::get('/contact', [HomeController::class, 'contact']);
 
 
 
