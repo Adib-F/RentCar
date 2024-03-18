846653eeaee279c6bbac5be4ff1e8e9ee198ab05
Arfa aniq <arfaaniq474@gmail.com>
2024-03-18 14:08:14 +0700

    Ini adalah view dan controller list produk untuk minggu 6

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListBarangController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/contact', [HomeController::class, 'contact']);
 
+Route::get('/img095', function(){
+    return view('img095');
+});
 
+Route::get('/listmobil5', [ListMobil5Controller::class, 'tampilkan']);
 
