b7faf5683e0d6add51395b60531386e7702f65ce
Adib-F <fakhrimuhammadadib@gmail.com>
2024-04-24 09:51:07 +0700

    Pembuatan Halaman Riwayat dan rental

--- resources/views/rental.blade.php
+++ resources/views/rental.blade.php
@@ -1,3 +1,68 @@
-<div>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
-</div>
+@extends('layout/app')
+
+@section('title')
+ Rental
+@endsection
+
+@section('navbar')
+  @include('components/navbar')
+@endsection
+
+@section('content')
+<body class="bg-gray-100 min-h-screen text-black">
+  <div class="card card-side bg-white shadow-xl w-[40rem] mx-auto mb-10 mt-10">
+    <div class="bg-orange-500 w-[15rem] h-[41.rem] flex items-center justify-center rounded-md">
+      <img src="images/cooper.png" alt="" class="w-[13rem] ml-5">
+    </div>
+    <div class="card-body d-flex">
+      <h2 class="font-bold text-2xl mb-3">Cooper Hardtop 4 Door</h2>
+      <form action="">
+        <div class="mb-2">
+          <label for="alamat1">Alamat</label>
+          <textarea class=" form-control form-control-sm border h-[4rem] border-black bg-white rounded-md" id="alamat1" required></textarea>
+        </div>
+        <div class="mb-2">
+          <label for="telepon1">No Telepon</label>
+          <input type="text"  class="form-control form-control-sm border border-black input input-xs input-bordered  w-[18.3rem] form-control rounded-md bg-white"  id="telepon1" required>
+        </div>
+        <div class="mb-2">
+          <label for="sim1">SIM</label>
+          <input type="file"  class="form-control form-control-sm border border-black file-input file-input-bordered file-input-xs h-8 flex w-[18.3rem] bg-white" />
+        </div>
+        <div class="mb-2">
+        <label for="mulaiTanggal" class="mb-1 block">Berapa Hari</label>
+        <div class="flex items-center">
+          <input type="date" class="form-control form-control-sm border border-black bg-white w-[8.6rem] rounded-md" id="mulaiTanggal">
+          <span class="mx-2"></span>
+          <input type="date" class="form-control form-control-sm border border-black bg-white w-[8.6rem] rounded-md" id="akhirTanggal">
+        </div>
+      </div>
+        <div class="mb-2">
+          <label for="Promo">Promo</label>
+          <select class="form-control form-control-sm  border border-black select-text w-[18.3rem] bg-white rounded-md" id="country" placeholder="Pilih Promo">
+            <option disable selected> Pilih Promo</option>
+            <option value="LB"> Promo Libur</option>
+            <option value="LR"> Promo Lebaran</option>
+          </select>
+        </div>
+        <div class="mb-2">
+          <label for="telepon1">Bayar Disini</label>
+          <button type="button" class="btn btn-sm btn-black flex text-white w-24" id="telepon1" required>QRIS</button>
+        </div>
+        <div class="mb-5">
+          <label for="sim1">Bukti Pembayaran</label>
+          <input type="file"  class="form-control form-control-sm border border-black file-input file-input-bordered file-input-xs h-8 flex w-[18.3rem] bg-white" />
+        </div>
+        <div class="flex items-center justify-end">
+        <button type="submit" class="btn btn-warning h-8 rounded-md">Rental Kendaraan</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</body>
+
+@endsection
+
+@section('footer')
+ @include('components/footer')
+@endsection
--- resources/views/riwayat.blade.php
+++ resources/views/riwayat.blade.php
@@ -1,3 +1,81 @@
-<div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-</div>
+@extends('layout/app')
+
+@section('title')
+ Riwayat
+@endsection
+
+@section('navbar')
+  @include('components/navbar')
+@endsection
+
+@section('content')
+<body class="bg-gray-100 min-h-screen text-black">
+  <div class="container mx-auto px-4 py-8 ">
+    <div class="row">
+      <div class="w-[70rem] mx-auto flex items-center mb-4 mt-10 justify-between bg-gray-300 rounded-lg shadow-sm ">
+        <div class="flex items-center ">
+          <img src="images/cooper.png" alt="" class="w-32 h-20 ml-3">
+          <div class="ml-3">
+            <p class="font-bold text-lg">Cooper Hardtop 4 door</p>
+            <p class="text-gray-600 text-sm">No Pesanan: 123456789</p>
+            <p class="text-gray-600 text-sm">Total Harga: 123.000</p>
+          </div>
+        </div>
+        <div class="flex items-center">
+        <a href="#" class="btn btn-sm border-none rounded-full px-2 py-1 mx-2 h-1 text-black bg-orange-400 hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-300">Detail Pesanan</a>
+        <a href="#" class="btn btn-sm border-none rounded-full px-2 py-1 mx-2 h-3 text-black bg-orange-400 hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-300">Cetak Resi</a>
+        </div>
+      </div>
+
+      <div class="w-[70rem] mx-auto flex items-center mb-4 justify-between bg-gray-300 rounded-md shadow-sm ">
+        <div class="flex items-center">
+          <img src="images/genio.png" alt="" class="w-28 h-20 ml-2">
+          <div class="ml-8">
+            <p class="font-bold text-lg">Honda Genio</p>
+            <p class="text-gray-600 text-sm">No Pesanan: 123456789</p>
+            <p class="text-gray-600 text-sm">Total Harga: 123.000</p>
+          </div>
+        </div>
+        <div class="flex items-center">
+        <a href="#" class="btn btn-sm border-none rounded-full px-2 py-1 mx-2 h-1 text-black bg-orange-400 hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-300">Detail Pesanan</a>
+        <a href="#" class="btn btn-sm border-none rounded-full px-2 py-1 mx-2 h-3 text-black bg-orange-400 hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-300">Cetak Resi</a>
+        </div>
+      </div>
+
+      <div class="w-[70rem] mx-auto flex items-center mb-4 justify-between bg-gray-300 rounded-md shadow-sm ">
+        <div class="flex items-center">
+          <img src="images/agya.png" alt="" class="w-32 h-20 m">
+          <div class="ml-6">
+            <p class="font-bold text-lg">Agya Gr Sport</p>
+            <p class="text-gray-600 text-sm">No Pesanan: 123456789</p>
+            <p class="text-gray-600 text-sm">Total Harga: 123.000</p>
+          </div>
+        </div>
+        <div class="flex items-center">
+        <a href="#" class="btn btn-sm border-none rounded-full px-2 py-1 mx-2 h-1 text-black bg-orange-400 hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-300">Detail Pesanan</a>
+        <a href="#" class="btn btn-sm border-none rounded-full px-2 py-1 mx-2 h-3 text-black bg-orange-400 hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-300">Cetak Resi</a>
+        </div>
+      </div>
+
+      <div class="w-[70rem] mx-auto flex items-center mb-10 justify-between bg-gray-300 rounded-lg shadow-sm ">
+        <div class="flex items-center">
+          <img src="images/beatstreet.png" alt="" class="w-30 h-20 ml-3 ">
+          <div class="ml-14">
+            <p class="font-bold text-lg">Honda Beat Street</p>
+            <p class="text-gray-600 text-sm">No Pesanan: 123456789</p>
+            <p class="text-gray-600 text-sm">Total Harga: 123.000</p>
+          </div>
+        </div>
+        <div class="flex items-center">
+        <a href="#" class="btn btn-sm border-none rounded-full px-2 py-1 mx-2 h-1 text-black bg-orange-400 hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-300">Detail Pesanan</a>
+        <a href="#" class="btn btn-sm border-none rounded-full px-2 py-1 mx-2 h-3 text-black bg-orange-400 hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-300">Cetak Resi</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+@endsection
+
+@section('footer')
+ @include('components/footer')
+@endsection
