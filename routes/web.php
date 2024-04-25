b37de52bc4ea68242514cd5f571f3f4ca42e82fb
Adib-F <fakhrimuhammadadib@gmail.com>
2024-04-25 21:36:54 +0700

    Pemberian Href

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,37 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\AboutusController;
+use App\Http\Controllers\CarsController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MotorcycleController;
+use App\Http\Controllers\RentalController;
+use App\Http\Controllers\RiwayatController;
+use App\Http\Controllers\RulesController;
+use App\Http\Controllers\SettingsController;
+
+
 
 Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('dashboard');
-});
 
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('/', [DashboardController::class, 'dashboard']);
+Route::get('/login', [LoginController::class, 'login']);
+Route::get('/register', [RegisterController::class, 'register']);
+Route::get('/cars', [CarsController::class, 'cars'])->name('cars');
+Route::get('/motorcycle', [MotorcycleController::class, 'motorcycle']);
+Route::get('/aboutus', [AboutusController::class, 'aboutus']);
+Route::get('/rulesandinfo', [RulesController::class, 'rules']);
+Route::get('/setting', [SettingsController::class, 'Settings']);
+Route::get('/riwayat', [RiwayatController::class, 'riwayat']);
+Route::get('/rental', [RentalController::class, 'rental']);
 
-Route::get('/register', function () {
-    return view('register');
-});
 
-Route::get('/setting', function () {
-    return view('setting');
-});
 
-Route::get('/aboutus', function () {
-    return view('aboutus');
-});
-
-Route::get('/rulesandinfo', function () {
-    return view('rulesandinfo');
-});
 
 Route::get('/statusbelumkonfirmasi', function () {
     return view('statusAwal');
@@ -38,23 +41,6 @@ Route::get('/statussetelahkonfirmasi', function () {
     return view('statusAkhir');
 });
 
-Route::get('/rental', function () {
-    return view('rental');
-});
-
-Route::get('/riwayat', function () {
-    return view('riwayat');
-});
-
 Route::get('/detailcars', function () {
     return view('detailcars');
 });
-
-Route::get('/cars', function () {
-    return view('cars');
-});
-
-Route::get('/motorcycle', function () {
-    return view('motorcycle');
-});
-
