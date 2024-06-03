325166ea039c7effa5b611f0e002a68c4e3998da
Gutserk <ardanasmirza@gmail.com>
2024-06-03 16:46:08 +0700

    update tampilan dashboard dan notif

--- resources/views/notifikasi.blade.php
+++ resources/views/notifikasi.blade.php
@@ -1,3 +1,19 @@
-<div>
+@extends('layout/app')
 
-</div>
+@section('title')
+ Beranda
+@endsection
+
+@section('navbar')
+  @include('components/navbar')
+@endsection
+
+@section('content')
+
+
+
+@endsection
+
+@section('footer')
+ @include('components/footer')
+@endsection
