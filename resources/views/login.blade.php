4b4a16fe938fce44fbf5a2ec24d876824a87456a
Adib-F <fakhrimuhammadadib@gmail.com>
2024-06-18 18:58:01 +0700

    Menambahkan alert

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,57 +1,56 @@
 @extends('layout.app')
 
 @section('title')
-  Login
+    Login
 @endsection
 
 @section('content')
+
 <body class="h-screen bg-gray-100">
     <div class="container flex h-screen">
-        <div class="relative w-1/2 bg-right bg-cover image-section" style="background-image: url('/images/rubicon.jpg')"></div>
-        <div class="absolute top-0 right-0 mt-8 mr-8">
-            <img src="/images/logo-rentcang.png" class="h-10 w-30 ">
+        <div class="relative w-1/2 bg-right bg-cover image-section" style="background-image: url('/images/rubicon.jpg')">
         </div>
+        <div class="absolute top-0 right-0 mt-8 mr-8">
+            <a href="{{ route('dashboard') }}">
+                <img src="/images/logo-rentcang.png" class="h-10 w-30">
+            </a>
+        </div>
+
         <div class="flex items-center justify-center w-1/2 p-8">
             <div class="w-5/6 p-8 bg-gray-100 rounded-lg">
-                <form action="{{route('login_proses')}}" method="POST">
-                @csrf
-                <div class="mb-4 text-5xl font-extrabold text-black">LOGIN</div>
-                    <div class="mt-4 text-xl font-bold text-black ">Belum memiliki akun? <a href="{{route('register')}}" class="text-blue-500 hover:text-blue-700">Disini</a>
-                    </div>
+                <form action="{{ route('login_proses') }}" method="POST" id="loginForm">
+                    @csrf
+                    <div class="mb-4 text-5xl font-extrabold text-black">LOGIN</div>
+                    <div class="mt-4 text-xl font-bold text-black">Belum memiliki akun? <a
+                            href="{{ route('register') }}" class="text-blue-500 hover:text-blue-700">Disini</a></div>
                     <br>
                     <div class="mb-4">
                         <label for="email" class="mb-2 font-semibold text-black">Email</label>
-                        <input type="email" id="email" name="email" class="w-full p-2 text-black bg-gray-300 border-gray-700 rounded-md hover:bg-gray-400 focus:outline-none focus:border-gray-700" autofocus required value="{{old('email')}}">
+                        <input type="email" id="email" name="email"
+                            class="w-full p-2 text-black bg-gray-300 border-gray-700 rounded-md hover:bg-gray-400 focus:outline-none focus:border-gray-700"
+                            autofocus required value="{{ old('email') }}">
                     </div>
-                    @error('email')
-                        <small>{{ $message }}</small>
-                    @enderror
                     <div class="relative mb-4">
                         <label for="password" class="mb-2 font-semibold text-black">Password</label>
-                        <input type="password" id="password" name="password" class="w-full p-2 text-black bg-gray-300 border-gray-700 rounded-md hover:bg-gray-400 focus:outline-none focus:border-gray-700" required>
-                        <img id="toggleIcon" src="images/eyeclose.svg" class="absolute w-6 cursor-pointer top-9 right-2" onclick="togglePasswordVisibility()">
+                        <input type="password" id="password" name="password"
+                            class="w-full p-2 text-black bg-gray-300 border-gray-700 rounded-md hover:bg-gray-400 focus:outline-none focus:border-gray-700"
+                            required>
+                        <img id="toggleIcon" src="images/eyeclose.svg" class="absolute w-6 cursor-pointer top-9 right-2"
+                            onclick="togglePasswordVisibility()">
                     </div>
-                    @error('password')
-                        <small>{{ $message }}</small>
-                    @enderror
-                   <button type="submit" class="px-4 py-1 font-semibold text-black bg-yellow-600 rounded-md hover:bg-yellow-500 font-poppins">Submit</button>
+                    <button type="submit"
+                        class="px-4 py-1 font-semibold text-black bg-yellow-600 rounded-md hover:bg-yellow-500 font-poppins">Submit</button>
                 </form>
             </div>
         </div>
     </div>
-    <script src=" https://cdn.jsdelivr.net/npm/sweetalert2@11.11.0/dist/sweetalert2.all.min.js "></script>
-
-    @error('email')
-    <script>
-            Swal.fire('{{ $message }}');
-        </script>
-    @enderror
-
-    @if ($message = session()->get('success'))
-        <script>
-            Swal.fire('{{ $message }}');
-        </script>
-    @endif
+    <div id="customAlert">
+        @if (session('berhasil'))
+            <div class="fixed px-4 py-2 text-white transform -translate-x-1/2 bg-green-500 rounded shadow-lg top-4 left-1/2">{{ session('berhasil') }}</div>
+        @elseif(session('gagal'))
+            <div class="fixed px-4 py-2 text-white transform -translate-x-1/2 bg-red-500 rounded shadow-lg top-4 left-1/2">{{ session('gagal') }}</div>
+        @endif
+    </div>
 
     <script>
         function togglePasswordVisibility() {
@@ -65,6 +64,15 @@
                 toggleIcon.src = 'images/eyeclose.svg';
             }
         }
+
+        document.addEventListener('DOMContentLoaded', function () {
+            const alertBox = document.querySelector('#customAlert > div');
+            if (alertBox) {
+                setTimeout(() => {
+                    alertBox.style.display = 'none';
+                }, 2000);
+            }
+        });
     </script>
 </body>
 @endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,63 +1,82 @@
-@extends('layout/app')
+@extends('layout.app')
 
 @section('title')
- Register
+    Register
 @endsection
 
 @section('content')
-<div class="flex">
-    <div class="w-1/2 bg-white">
-        <div class="justify-start">
-            <img src="images/logo-rentcang.png" class="w-40 ml-8" alt="">
-        </div>
-        <div class="grid justify-center mt-8 ">
-            <form action="{{ route('register_proses') }}" method="POST">
-                @csrf
-                <h1 class="text-3xl font-extrabold">DAFTAR</h1>
-                <p class="mb-8 text-base font-semibold">Sudah memiliki akun?<a href="{{ route('login') }}"
-                        class="text-lg text-orange-400"> Masuk</a></p>
-                <p class="text-sm font-bold font-poppins">Nama Lengkap </p>
-                <input type="text" name="nama_lengkap" class="mb-2 text-sm bg-gray-300 rounded-sm h-7 w-80">
-                <p class="text-sm font-bold font-poppins">Username </p>
-                <input type="text" name="username" class="mb-2 text-sm bg-gray-300 rounded-sm h-7 w-80">
-                <p class="text-sm font-bold font-poppins">No.Handphone </p>
-                <input type="text" name="no_handphone" class="mb-2 bg-gray-300 rounded-sm h-7 w-80">
-                <p class="text-sm font-bold font-poppins">NIK </p>
-                <input type="text" name="nik" class="mb-2 bg-gray-300 rounded-sm h-7 w-80">
-                <p class="text-sm font-bold font-poppins">Email </p>
-                <input type="email" name="email" class="mb-2 bg-gray-300 rounded-sm h-7 w-80">
-                <p class="text-sm font-bold font-poppins">Password </p>
-                <div class="relative mb-4 w-80">
-                    <input id="password" type="password" name="password" class="w-full pr-10 bg-gray-300 rounded-sm h-7">
-                    <img id="toggleIcon" src="images/eyeclose.svg"
-                        class="absolute w-6 transform -translate-y-1/2 cursor-pointer top-1/2 right-2"
-                        onclick="togglePasswordVisibility()">
-                </div>
-                <div class="grid justify-start">
-                    <button class="w-20 h-10 text-xs font-extrabold bg-orange-400 rounded-md">Daftar</button>
-                </div>
-            </form>
-        </div>
-    </div>
+    <div class="flex">
+        <div class="w-1/2 bg-white">
+            <div class="justify-start">
+                <img src="images/logo-rentcang.png" class="w-40 ml-8" alt="">
+            </div>
+            <div class="grid justify-center mt-8 ">
+                <form action="{{ route('register_proses') }}" method="POST" id="registerForm">
+                    @csrf
+                    <h1 class="text-3xl font-extrabold">DAFTAR</h1>
+                    <p class="mb-8 text-base font-semibold">Sudah memiliki akun?<a href="{{ route('login') }}"
+                            class="text-lg text-orange-400"> Masuk</a></p>
 
-    <div class="w-1/2">
-        <img src="images/rubicon.jpg" class="w-full min-h-screen" alt="">
+                    <p class="text-sm font-bold font-poppins">Nama Lengkap </p>
+                    <input type="text" name="nama_lengkap" class="mb-2 text-sm bg-gray-300 rounded-sm h-7 w-80" value="{{ old('nama_lengkap') }}">
+
+                    <p class="text-sm font-bold font-poppins">Username </p>
+                    <input type="text" name="username" class="mb-2 text-sm bg-gray-300 rounded-sm h-7 w-80" value="{{ old('username') }}">
+
+                    <p class="text-sm font-bold font-poppins">No.Handphone </p>
+                    <input type="text" name="no_handphone" class="mb-2 bg-gray-300 rounded-sm h-7 w-80" value="{{ old('no_handphone') }}">
+
+                    <p class="text-sm font-bold font-poppins">NIK </p>
+                    <input type="text" name="nik" class="mb-2 bg-gray-300 rounded-sm h-7 w-80" value="{{ old('nik') }}">
+
+                    <p class="text-sm font-bold font-poppins">Email </p>
+                    <input type="email" name="email" class="mb-2 bg-gray-300 rounded-sm h-7 w-80" value="{{ old('email') }}">
+
+                    <p class="text-sm font-bold font-poppins">Password </p>
+                    <div class="relative mb-4 w-80">
+                        <input id="password" type="password" name="password"
+                            class="w-full pr-10 bg-gray-300 rounded-sm h-7">
+                        <img id="toggleIcon" src="images/eyeclose.svg"
+                            class="absolute w-6 transform -translate-y-1/2 cursor-pointer top-1/2 right-2"
+                            onclick="togglePasswordVisibility()">
+                    </div>
+                    <div class="grid justify-start">
+                        <button type="submit" class="w-20 h-10 text-xs font-extrabold bg-orange-400 rounded-md">Daftar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="w-1/2">
+            <img src="images/rubicon.jpg" class="w-full min-h-screen" alt="">
+        </div>
     </div>
-</div>
-<script>
-    function togglePasswordVisibility() {
-        const passwordInput = document.getElementById('password');
-        const toggleIcon = document.getElementById('toggleIcon');
-        if (passwordInput.type === 'password') {
-            passwordInput.type = 'text';
-            toggleIcon.src = 'images/eyeopen.svg';
-        } else {
-            passwordInput.type = 'password';
-            toggleIcon.src = 'images/eyeclose.svg';
+    <div id="customAlert">
+        @if (session('berhasil'))
+            <div class="fixed px-4 py-2 text-white transform -translate-x-1/2 bg-green-500 rounded shadow-lg top-4 left-1/2">{{ session('berhasil') }}</div>
+        @elseif(session('gagal'))
+            <div class="fixed px-4 py-2 text-white transform -translate-x-1/2 bg-red-500 rounded shadow-lg top-4 left-1/2">{{ session('gagal') }}</div>
+        @endif
+    </div>
+    <script>
+        function togglePasswordVisibility() {
+            const passwordInput = document.getElementById('password');
+            const toggleIcon = document.getElementById('toggleIcon');
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                toggleIcon.src = 'images/eyeopen.svg';
+            } else {
+                passwordInput.type = 'password';
+                toggleIcon.src = 'images/eyeclose.svg';
+            }
         }
-    }
-</script>
-</body>
+
+        document.addEventListener('DOMContentLoaded', function () {
+            const alertBox = document.querySelector('#customAlert > div');
+            if (alertBox) {
+                setTimeout(() => {
+                    alertBox.style.display = 'none';
+                }, 5000);
+            }
+        });
+    </script>
 @endsection
-
-
